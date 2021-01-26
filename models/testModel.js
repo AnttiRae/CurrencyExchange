6042ed786684988cc6356bb3269befb0954272af
Antti Rae <antti.rae@edu.turkuamk.fi>
2021-01-26 14:26:27 +0200

    add schema for currency rates
    use new schema to test and create a model in a new route

--- models/testModel.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-const {Schema} = mongoose;
-
-const latestCurrencySchema = new Schema({
-    title: String,
-
-})
---
+++ routes/currencyExchangeAPI.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var { mongoose } = require('mongoose');
+var { latestCurrencyModel } = require('../schemas/currencySchema.js')
+var router = express.Router();
+
+/* GET latest currency rates. */
+router.get('/latest', function(req, res, next) {
+  
+  const example = latestCurrencyModel({
+    timestamp: 1611648000,
+    base: "USD",
+  })
+  console.log(example.base)
+  example.save(function(err, exmaple) {
+    if (err) { return console.log(err)}
+  });
+  
+  res.json({
+    "message": "reached currency api"
+  });
+});
+
+module.exports = router;
